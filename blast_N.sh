18020b4a22be5897d6648307d6b8b92af4209c93
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 19:45:58 -0400

    Add blast_N.sh

---
+++ blast_N.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for contig in $@
+do
+    outname=$(basename $contig)
+
+    nohup blastn \
+    -task megablast \
+    -query $contig \
+    -db /home/share/databases/ncbi_nt/nt \
+    -outfmt '6 qseqid staxids bitscore std sscinames sskingdoms stitle' \
+    -culling_limit 5 \
+    -max_target_seqs 10 \
+    -num_threads 48 \
+    -evalue 1e-5 \
+     -out $outname.vs.nt.cul5.maxt10.1e5.megablast.out &
+done
