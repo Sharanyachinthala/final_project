ceda9eddf8a1f117d5b508b33784b3582a366382
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 21:59:16 -0400

    Add blast_N.sh

--- blast_N.sh
+++ blast_N.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Run BLAST megablast for each input contig file
 
-for contig in "$@"
-do
+for contig in "$@"; do
+    # Get the base name of the contig file (remove path)
     outname=$(basename "$contig")
 
+    # Run BLASTn (megablast) with specified parameters in background (nohup &)
     nohup blastn \
     -task megablast \
     -query "$contig" \
@@ -13,5 +15,5 @@ do
     -max_target_seqs 10 \
     -num_threads 48 \
     -evalue 1e-5 \
-     -out "$outname.vs.nt.cul5.maxt10.1e5.megablast.out" &
+    -out "$outname.vs.nt.cul5.maxt10.1e5.megablast.out" &
 done
