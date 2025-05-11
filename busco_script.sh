5fce96a85feeff54dc8680a81c5849068408c7fd
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 22:06:50 -0400

    Add busco_script.sh

--- busco_script.sh
+++ busco_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Run BUSCO genome completeness analysis
+
+# Activate conda environment for BUSCO
 source activate busco-env
 
+# Set input variables: genome assembly file and output folder
 contigs=$1
 output=$2
 
+# Run BUSCO with 24 threads using bacteria dataset
 busco -i "$contigs" -m genome -o "$output" -l bacteria_odb10 -c 24
