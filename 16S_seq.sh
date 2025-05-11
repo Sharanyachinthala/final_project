51c8518339cf9ad50b4a8838baea956fe2ba5ad5
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 21:48:38 -0400

    Add 16S_seq.sh

--- 16S_seq.sh
+++ 16S_seq.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Extract 16S rRNA sequences from all .ffn files
+
 for file in ./*.ffn; do
-    extract_sequences "16S ribosomal RNA" "$file" > 16S_sequence.fasta
+	#Extract 16S sequences from each file
+    	extract_sequences "16S ribosomal RNA" "$file" > 16S_sequence.fasta
 done
