549e25142ee97e62e09dad4b6f695532336476a8
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 21:54:55 -0400

    Add base_reads.sh

--- base_reads.sh
+++ base_reads.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# Calculate sequencing read coverage for all R1 and R2 FASTQ files
 
 for fastq in ./*_R1_*  ./*_R2_*;
 do
-echo file: $fastq
+	# Print current file name
+	echo file: $fastq
 
+	# Count number of reads (lines starting with @ in compressed FASTQ)
+	readcount=$(zgrep -c '^@' $fastq )
+	echo readcount: $readcount
 
-readcount=$(zgrep -c '^@' $fastq )
-echo readcount: $readcount
-total_bp=$((readcount * 250 * 2))
-echo total_bp: $total_bp
-coverage=$((total_bp/7000000))
-echo coverage: $coverage
+	# Calculate total base pairs (read count x 250 bp x 2 for paired-end)
+	total_bp=$((readcount * 250 * 2))
+	echo total_bp: $total_bp
 
-
+	# Estimate coverage assuming 7 Mb genome size
+	coverage=$((total_bp/7000000))
+	echo coverage: $coverage
 done
