33656f72c98e74e044f3b96917ec8c139afa5bc9
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 22:24:12 -0400

    Add Quast Script

--- quast_script.sh
+++ quast_script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Run QUAST to assess genome assembly quality
+
+# Activate the quast conda environment
 source activate quast-env
 
+# Set input variables: contig file and output directory
 contigs=$1
 output=$2
 
+# Run QUAST with specified input and output
 quast.py "$contigs" -o "$output"
