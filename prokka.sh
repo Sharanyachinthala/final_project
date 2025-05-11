6e7f0bb72a1925f470a9cae817d73e2f6cf00472
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 22:09:22 -0400

    Add prokka.sh

--- prokka.sh
+++ prokka.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# Run PROKKA for genome annotation
 
+# Activate the genomics conda environment
 source activate genomics
 
+# Define output directory and input file
 out_dir=prokka_output
 file=contigs.fasta
 
+# Run PROKKA in background with nohup
 nohup prokka "$file" --outdir "$out_dir" --cpus 24 --mincontiglen 200 &
+
