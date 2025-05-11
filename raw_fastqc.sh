c8adace2b1e822031b9b8731d2cc8648f30f6ece
sc1830 <sc1830@ron.sr.unh.edu>
2025-05-10 22:40:54 -0400

    Add raw_fastqc.sh

--- raw_fastqc.sh
+++ raw_fastqc.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-# Step 1: Quality check using FastQC
+# Step 1: Quality check of raw reads using FastQC
+
+# Activate the genomics conda environment
 source activate genomics
 
+# Create output directory if it doesn't exist
 mkdir -p fastqc_raw
 
+# Run FastQC on all raw FASTQ files and save results to output folder
 fastqc data/raw/*.fastq.gz -o fastqc_raw
